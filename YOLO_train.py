558b56ce1da0b2029fa410a6707389bc638e81c3
joshkeselman <jhkeselman@wpi.edu>
2025-03-15 17:14:46 -0400

    just adding windows as an option. its still slow as hell without CUDA

--- YOLO_train.py
+++ YOLO_train.py
@@ -1,4 +1,7 @@
 from ultralytics import YOLO
 
 model = YOLO('yolo12n.pt')
-results = model.train(data="/home/jhkeselman/cleaningoceanplastics_resources/datasets/version1/data.yaml", epochs=100, imgsz=640)
+# Josh's Linux
+# results = model.train(data='/home/jhkeselman/Downloads/dataset/data.yaml', epochs=100, imgsz=640)
+# Josh's Windows
+results = model.train(data='D:/cleaningoceanplastics_resources/datasets/version1/data.yaml', epochs=100, imgsz=640)
