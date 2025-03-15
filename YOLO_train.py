97a4c4d45c1015b8efed95d39e1e4189c011e2fd
jhkeselman <jhkeselman@wpi.edu>
2025-03-15 00:19:08 -0400

    saving updated training code

--- YOLO_train.py
+++ YOLO_train.py
@@ -1,5 +1,4 @@
 from ultralytics import YOLO
 
-model = YOLO('yolo11n.pt')
-results = model.train(data='/home/jhkeselman/Downloads/dataset/data.yaml', epochs=100, imgsz=640)
-model.save('/home/jhkeselman/cleaningoceanplastics_resources')
+model = YOLO('yolo12n.pt')
+results = model.train(data="/home/jhkeselman/cleaningoceanplastics_resources/datasets/version1/data.yaml", epochs=100, imgsz=640)
