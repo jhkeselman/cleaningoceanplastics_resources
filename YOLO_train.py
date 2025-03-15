d7faadc1db6d437edbca30302081f3794a8d88ab
Joshua Keselman <jhkeselman@login-03.turing.wpi.edu>
2025-03-15 17:55:33 -0400

    pu pushing from turing for safety

--- YOLO_train.py
+++ YOLO_train.py
@@ -1,7 +1,7 @@
 from ultralytics import YOLO
 
 model = YOLO('yolo12n.pt')
-# Josh's Linux
-# results = model.train(data='/home/jhkeselman/Downloads/dataset/data.yaml', epochs=100, imgsz=640)
+# Josh's Turing
+results = model.train(data='/home/jhkeselman/cleaningoceanplastics_resources/datasets/version1/data.yaml', epochs=100, imgsz=640)
 # Josh's Windows
-results = model.train(data='D:/cleaningoceanplastics_resources/datasets/version1/data.yaml', epochs=100, imgsz=640)
+# results = model.train(data='D:/cleaningoceanplastics_resources/datasets/version1/data.yaml', epochs=100, imgsz=640)
