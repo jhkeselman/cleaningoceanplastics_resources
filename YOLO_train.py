9d79a97c66db199b72b865c7bbb28c355f42df90
jhkeselman <jhkeselman@wpi.edu>
2025-03-20 11:11:05 -0400

    version2

--- YOLO_train.py
+++ YOLO_train.py
@@ -2,6 +2,6 @@ from ultralytics import YOLO
 
 model = YOLO('yolo12n.pt')
 # Josh's Turing
-results = model.train(data='/home/jhkeselman/cleaningoceanplastics_resources/datasets/version1/data.yaml', epochs=100, imgsz=640)
+results = model.train(data='/home/jhkeselman/cleaningoceanplastics_resources/datasets/version2/data.yaml', epochs=100, imgsz=640)
 # Josh's Windows
 # results = model.train(data='D:/cleaningoceanplastics_resources/datasets/version1/data.yaml', epochs=100, imgsz=640)
