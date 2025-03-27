f3086d59a96c262546426d4941f7bfb4afb2402f
jhkeselman <jhkeselman@wpi.edu>
2025-03-26 23:53:55 -0400

    you have yourself a family feud

--- YOLO_train.py
+++ YOLO_train.py
@@ -2,6 +2,6 @@ from ultralytics import YOLO
 
 model = YOLO('yolo12n.pt')
 # Josh's Turing
-results = model.train(data='/home/jhkeselman/cleaningoceanplastics_resources/datasets/version5/data.yaml', epochs=100, imgsz=640, patience=100, lr0=0.005)
+results = model.train(data='/home/jhkeselman/cleaningoceanplastics_resources/datasets/version5/data.yaml', epochs=100, imgsz=640)
 # Josh's Windows
 # results = model.train(data='D:/cleaningoceanplastics_resources/datasets/version1/data.yaml', epochs=100, imgsz=640)
