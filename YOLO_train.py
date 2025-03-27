6fbd916f8e19fe22d5075bb6f33d0fdaa0808d34
jhkeselman <jhkeselman@wpi.edu>
2025-03-27 08:48:57 -0400

    transfer from v4 to mqp images?

--- YOLO_train.py
+++ YOLO_train.py
@@ -1,7 +1,7 @@
 from ultralytics import YOLO
 
-model = YOLO('yolo12n.pt')
+model = YOLO('/home/jhkeselman/cleaningoceanplastics_resources/runs/detect/train4/weights/best.pt')
 # Josh's Turing
-results = model.train(data='/home/jhkeselman/cleaningoceanplastics_resources/datasets/version5/data.yaml', epochs=100, imgsz=640)
+results = model.train(data='/home/jhkeselman/cleaningoceanplastics_resources/datasets/version5/data.yaml', epochs=200, imgsz=640)
 # Josh's Windows
 # results = model.train(data='D:/cleaningoceanplastics_resources/datasets/version1/data.yaml', epochs=100, imgsz=640)
