a1ab5967d14965510f85483f3f3d28c45754eea3
jhkeselman <jhkeselman@wpi.edu>
2025-03-26 23:09:59 -0400

    trying transfer learning

--- YOLO_train.py
+++ YOLO_train.py
@@ -1,7 +1,7 @@
 from ultralytics import YOLO
 
-model = YOLO('yolo12n.pt')
+model = YOLO('/home/jhkeselman/cleaningoceanplastics_resources/runs/detect/train6/weights/best.pt')
 # Josh's Turing
-results = model.train(data='/home/jhkeselman/cleaningoceanplastics_resources/datasets/version4/data.yaml', epochs=200, imgsz=640, patience=100, lr0=0.005)
+results = model.train(data='/home/jhkeselman/cleaningoceanplastics_resources/datasets/version3/data.yaml', epochs=100, imgsz=640, patience=100, lr0=0.005)
 # Josh's Windows
 # results = model.train(data='D:/cleaningoceanplastics_resources/datasets/version1/data.yaml', epochs=100, imgsz=640)
