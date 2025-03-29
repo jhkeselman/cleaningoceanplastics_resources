ce2acbc15b764246f0b80ef1722c8afc1e996a9b
jhkeselman <jhkeselman@wpi.edu>
2025-03-28 22:07:18 -0400

    new training, full mqp

--- YOLO_train.py
+++ YOLO_train.py
@@ -1,7 +1,7 @@
 from ultralytics import YOLO
 
-model = YOLO('/home/jhkeselman/cleaningoceanplastics_resources/runs/detect/train9/weights/best.pt')
+model = YOLO('/home/jhkeselman/cleaningoceanplastics_resources/runs/detect/train11/weights/best.pt')
 # Josh's Turing
-results = model.train(data='/home/jhkeselman/cleaningoceanplastics_resources/datasets/version5/data.yaml', epochs=200, imgsz=640, device=[0,1], lr0=0.005)
+results = model.train(data='/home/jhkeselman/cleaningoceanplastics_resources/datasets/version6/data.yaml', epochs=200, imgsz=640, device=[0,1], lr0=0.005)
 # Josh's Windows
 # results = model.train(data='D:/cleaningoceanplastics_resources/datasets/version1/data.yaml', epochs=100, imgsz=640)
