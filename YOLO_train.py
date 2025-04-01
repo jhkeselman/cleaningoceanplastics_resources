48bd278e42b44d2b81f9ab0c1cdd6afa98633cb5
jhkeselman <jhkeselman@wpi.edu>
2025-03-31 23:11:39 -0400

    longer timeline

--- YOLO_train.py
+++ YOLO_train.py
@@ -2,6 +2,6 @@ from ultralytics import YOLO
 
 model = YOLO('/home/jhkeselman/cleaningoceanplastics_resources/runs/detect/train11/weights/best.pt')
 # Josh's Turing
-results = model.train(data='/home/jhkeselman/cleaningoceanplastics_resources/datasets/version6/data.yaml', epochs=200, imgsz=640, device=[0,1], lr0=0.005)
+results = model.train(data='/home/jhkeselman/cleaningoceanplastics_resources/datasets/version6/data.yaml', epochs=500, imgsz=640, device=[0,1])
 # Josh's Windows
 # results = model.train(data='D:/cleaningoceanplastics_resources/datasets/version1/data.yaml', epochs=100, imgsz=640)
