b39a9337657e8e2b9eaa471f4860add78f7d114e
jhkeselman <jhkeselman@wpi.edu>
2025-03-31 23:15:44 -0400

    oops i cannot read my own name changes

--- YOLO_train.py
+++ YOLO_train.py
@@ -1,6 +1,6 @@
 from ultralytics import YOLO
 
-model = YOLO('/home/jhkeselman/cleaningoceanplastics_resources/runs/detect/train11/weights/best.pt')
+model = YOLO('/home/jhkeselman/cleaningoceanplastics_resources/runs/detect/train11/weights/bestv11.pt')
 # Josh's Turing
 results = model.train(data='/home/jhkeselman/cleaningoceanplastics_resources/datasets/version6/data.yaml', epochs=500, imgsz=640, device=[0,1])
 # Josh's Windows
