1afcab8f579dc29904ab05c939d2473a7d25a06f
jhkeselman <jhkeselman@wpi.edu>
2025-04-04 14:40:02 -0400

    dots on corners

--- YOLO_boxes.py
+++ YOLO_boxes.py
@@ -1,14 +1,16 @@
 from ultralytics import YOLO
 import cv2
 
-model = YOLO('yolo11n.pt')
+model = YOLO('yolo12n.pt')
 
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(2)
 
 while cap.isOpened():
     ret, frame = cap.read()
     if not ret:
         break
+
+    frame = cv2.flip(frame, 0)
 
     results = model(frame)
 
@@ -19,7 +21,10 @@ while cap.isOpened():
         class_label = model.names[int(box.cls[0])]
         raw_conf = box.conf[0]
         conf = 100 * round(raw_conf.item(), 2)
-        print(f"Detected {class_label} at [{x1}, {y1}, {x2}, {y2}] with {conf}% confidence")
+        objectString = f"{class_label}, {conf}, {x1}, {y1}, {x2}, {y2}"
+        cv2.circle(annotated_frame, (x1, y1), 5, (0,0,255), -1)
+        cv2.circle(annotated_frame, (x2, y2), 5, (255,0,0), -1)
+        print(objectString)
 
     cv2.imshow("YOLO Real-Time Detection", annotated_frame)
 
